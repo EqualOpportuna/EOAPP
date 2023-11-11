311afadab00755f607fda8aabb62a8c0047f9278
eefei22 <117620367+eefei22@users.noreply.github.com>
2023-11-11 20:40:14 +0800

    sign up page minor fix
    tried to change button color but doesn't work

--- app/src/main/java/com/example/equalopportuna/sign_up.java
+++ app/src/main/java/com/example/equalopportuna/sign_up.java
@@ -13,7 +13,7 @@ public class sign_up extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up); //
+        setContentView(R.layout.sign_up);
 
         // Bind XML views to Java objects
         signUpButton = findViewById(R.id.btn_register);
