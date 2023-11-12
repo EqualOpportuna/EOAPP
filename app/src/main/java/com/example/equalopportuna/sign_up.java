8963d36f6cf67e80870b64fd8331ab4af8a68cf6
Navid <navidrazavi22@gmail.com>
2023-11-12 17:05:42 +0800

    change login look, added MainPage

--- app/src/main/java/com/example/equalopportuna/sign_up.java
+++ app/src/main/java/com/example/equalopportuna/sign_up.java
@@ -1,9 +1,11 @@
 package com.example.equalopportuna;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +31,13 @@ public class sign_up extends AppCompatActivity {
         etPassword = findViewById(R.id.et_password);
         etConfPassword = findViewById(R.id.et_confPassword);
 
+        TextView orLoginTextView = findViewById(R.id.or_login);
+        orLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(sign_up.this, login_page.class));
+            }
+        });
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
