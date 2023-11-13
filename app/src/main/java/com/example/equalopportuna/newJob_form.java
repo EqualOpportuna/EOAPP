6e7e72dd06f7e31a2a56d4aee8b0acb851af7e7e
eefei22 <117620367+eefei22@users.noreply.github.com>
2023-11-13 16:43:19 +0800

    Create a form to post new vacancies
    Create a form to post new vacancies

---
+++ app/src/main/java/com/example/equalopportuna/newJob_form.java
@@ -0,0 +1,14 @@
+package com.example.equalopportuna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newJob_form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newjob_form);
+    }
+}
