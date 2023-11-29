002c67ffd277ad9277bf727d6882744c5cc18488
DexterWithouP <119928716+ko-kusang@users.noreply.github.com>
2023-11-30 02:59:15 +0800

    backing

--- app/src/main/java/com/example/equalopportuna/courselistingFragment.java
+++ app/src/main/java/com/example/equalopportuna/courselistingFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.content.Context;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -49,7 +50,7 @@ public class courselistingFragment extends Fragment {
 
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new CL_Adapter(getApplicationContext()));
+        recyclerView.setAdapter(new CL_Adapter());
 
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
