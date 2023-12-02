68677d38ede2cd0c9dfae5e51e06a15c3813d5b0
eefei22 <117620367+eefei22@users.noreply.github.com>
2023-12-02 22:41:12 +0800

    connection module updates
    - Updated user tab UI
    - Updated friends tab UI
    - new friends_adapter class
    - Tested the layout with dummy resources

--- app/src/main/java/com/example/equalopportuna/Friends_adapter.java
+++ app/src/main/java/com/example/equalopportuna/Friends_adapter.java
@@ -1,4 +1,5 @@
 package com.example.equalopportuna;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,16 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.equalopportuna.R;
-import com.example.equalopportuna.Users;
+import com.example.equalopportuna.Friends;
 
 import java.util.List;
 
-public class user_adapter extends RecyclerView.Adapter<user_adapter.ViewHolder> {
+public class Friends_adapter extends RecyclerView.Adapter<Friends_adapter.ViewHolder> {
 
     private final Context context;
-    private final List<Users> userList;
+    private final List<Friends> userList;
 
-    public user_adapter(Context context, List<Users> userList) {
+    public Friends_adapter(Context context, List<Friends> userList) {
         this.context = context;
         this.userList = userList;
     }
@@ -28,20 +29,21 @@ public class user_adapter extends RecyclerView.Adapter<user_adapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_connections_users, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_connections_friends, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Users user = userList.get(position);
+        Friends user = userList.get(position);
 
         // Set data to views
         holder.profilePic.setImageResource(user.getImageUrl());
         holder.username.setText(user.getUsername());
         holder.career_desc.setText(user.getCareerDescription());
+        holder.connection_period.setText("Connected " + user.getConnectionPeriod() + " months ago");
 
-        // set button handler
+        //set onClickListener for button
     }
 
     @Override
@@ -51,15 +53,16 @@ public class user_adapter extends RecyclerView.Adapter<user_adapter.ViewHolder> 
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView profilePic;
-        TextView username, career_desc;
-        Button BTNconnect;
+        TextView username, career_desc, connection_period;
+        Button chatBTN;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             profilePic = itemView.findViewById(R.id.profilePic);
             username = itemView.findViewById(R.id.username);
             career_desc = itemView.findViewById(R.id.career_desc);
-            BTNconnect = itemView.findViewById(R.id.BTNconnect);
+            connection_period = itemView.findViewById(R.id.connection_period);
+            chatBTN = itemView.findViewById(R.id.chatBTN);
         }
     }
 }
--- app/src/main/java/com/example/equalopportuna/Users.java
+++ app/src/main/java/com/example/equalopportuna/Users.java
@@ -6,9 +6,10 @@ import java.util.List;
 public class Users {
     private String username;
     private String careerDescription;
-    private String imageUrl;
+    private int imageUrl;   //atm using int because i just wanna test things out; change back to strings later
 
-    public Users(String username, String careerDescription, String imageUrl) {
+
+    public Users(String username, String careerDescription, int imageUrl) {
         this.username = username;
         this.careerDescription = careerDescription;
         this.imageUrl = imageUrl;
@@ -22,15 +23,17 @@ public class Users {
         return careerDescription;
     }
 
-    public String getImageUrl() {
+    public int getImageUrl() {
         return imageUrl;
     }
     public static List<Users> getUserList() {
         List<Users> userList = new ArrayList<>();
 
+        // testing with sample data
+        userList.add(new Users("Ali Abu Bakar", "Engineer", R.drawable.profile_image1));
+        userList.add(new Users("Ching Ching Mai", "Designer", R.drawable.profile_image2));
+        userList.add(new Users("Muthu", "Developer", R.drawable.profile_image3));
+
         return userList;
     }
-    Users user1 = new Users("JohnDoe", "Software Engineer", "https://www.gstatic.com/android/keyboard/emojikitchen/20221101/u1f98a/u1f98a_u1f920.png?fbx");
-    Users user2 = new Users("AliceSmith", "Graphic Designer", "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f60e/u1f60e_u1f435.png?fbx");
-
 }
