0c8b08b8db7ecf174143783c46d7d75af9b7ef01
wlsoo1234 <s2148250@siswa.um.edu.my>
2023-12-09 06:32:39 +0800

    deleting forgot password

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.0" apply false
 
 }
