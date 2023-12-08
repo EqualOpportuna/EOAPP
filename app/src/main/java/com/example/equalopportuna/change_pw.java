caae1dcd2ab6def2ac61dfb4cc31e37e2906200c
wlsoo1234 <s2148250@siswa.um.edu.my>
2023-12-09 04:04:29 +0800

    change_pw adding request

--- app/src/main/java/com/example/equalopportuna/change_pw.java
+++ app/src/main/java/com/example/equalopportuna/change_pw.java
@@ -12,8 +12,13 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class change_pw extends AppCompatActivity {
-    private EditText etconfNewPassword,etnewpassword,etcurrentpw;
+    private EditText etemail,etconfNewPassword,etnewpassword,etcurrentpw;
     private Button btnproceed,btnback;
 
     @SuppressLint("WrongViewCast")
@@ -21,6 +26,7 @@ public class change_pw extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_pw);
 
+        etemail = findViewById(R.id.et_email);
         etcurrentpw = findViewById(R.id.et_currentpw);
         etnewpassword = findViewById(R.id.et_newpassword);
         etconfNewPassword = findViewById(R.id.et_confNewPassword);
@@ -37,19 +43,26 @@ public class change_pw extends AppCompatActivity {
         btnproceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(validateinputs()){
+                    changeDatainDatabase();
+                }
             }
         });
     }
 
     private boolean validateinputs() {
+        String email = etemail.getText().toString();
         String password = etcurrentpw.getText().toString();
         String NewPassword = etnewpassword.getText().toString();
         String confNewPassword = etconfNewPassword.getText().toString();
 
-
-        if (password.isEmpty() || NewPassword.isEmpty() || confNewPassword.isEmpty()) {
+        if (email.isEmpty() || password.isEmpty() || NewPassword.isEmpty() || confNewPassword.isEmpty()) {
             showToast("Please fill in all fields");
+            return false;
+        }
+
+        if (isEmailRegistered(email)) {
+            showToast("Email is already registered");
             return false;
         }
 
@@ -74,6 +87,76 @@ public class change_pw extends AppCompatActivity {
     private boolean isValidPassword(String password) {
         // Password must be at least 5 characters, contain at least one number, and one special symbol
         return password.length() >= 5 && password.matches(".*\\d.*") && password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*");
+    }
+
+
+    private boolean isEmailRegistered(String email) {
+        Database database = new Database();
+        Connection connection = database.SQLConnection();
+
+        if (connection != null) {
+            try {
+                String query = "SELECT * FROM users WHERE email = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, email);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if (resultSet.next()) {
+                    preparedStatement.close();
+                    connection.close();
+                    return true;
+                }
+
+                preparedStatement.close();
+                connection.close();
+
+            } catch (SQLException e) {
+                showToast("Error checking email in the database: " + e.getMessage());
+            }
+        } else {
+            showToast("Error connecting to the database");
+        }
+
+        return false;
+    }
+
+
+
+    private void changeDatainDatabase() {
+        String email = etemail.getText().toString().trim();
+        String currentpw = etcurrentpw.getText().toString().trim();
+        String newpw = etnewpassword.getText().toString().trim();
+
+        Database database = new Database();
+        Connection connection = database.SQLConnection();
+
+        if (connection != null) {
+            try {
+                String query0 = "UPDATE users \n SET password = ? \n WHERE email = ?";
+                /* String query1 = "SET password = '" + newpw + "'";
+                String query2 = "WHERE email = " + email; */
+                PreparedStatement preparedStatement = connection.prepareStatement(query0);
+                preparedStatement.setString(1, newpw);
+                preparedStatement.setString(2, email);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    showToast("Password Changed!");
+                } else {
+                    showToast("Changing password failed");
+                }
+
+                preparedStatement.close();
+                connection.close();
+
+            } catch (SQLException e) {
+                showToast("Error inserting data into the database: " + e.getMessage());
+            }
+        } else {
+            showToast("Error connecting to the database");
+        }
     }
 
     private void showToast(String message) {
