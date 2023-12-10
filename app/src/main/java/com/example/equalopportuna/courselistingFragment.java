cf1455a433648ecc68d9b52f3b5e3ec7871fe94b
XcloudFance <34064977+XcloudFance@users.noreply.github.com>
2023-12-10 19:33:57 +0800

    course list getting

--- app/src/main/java/com/example/equalopportuna/courselistingFragment.java
+++ app/src/main/java/com/example/equalopportuna/courselistingFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.content.Context;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,10 +16,15 @@ import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import android.widget.Toast;
 public class courselistingFragment extends Fragment {
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +40,6 @@ public class courselistingFragment extends Fragment {
 
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,8 +48,37 @@ public class courselistingFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.CourseRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        List<String> courseNames = new ArrayList<>();
+        List<String> courseDates = new ArrayList<>();
+        List<String> courseFeedbacks = new ArrayList<>();
+        Database database = new Database();
+        Connection connection = database.SQLConnection();
+        String query = "SELECT course_name, course_date, course_feedback FROM courses";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                // 获取每一行的数据
+                String courseName = resultSet.getString("course_name");
+                String courseDate = resultSet.getString("course_date");
+                String courseFeedback = resultSet.getString("course_feedback");
+                System.out.println(courseName);
+
+                // 将数据存入相应的列表
+                courseNames.add(courseName);
+                courseDates.add(courseDate);
+                courseFeedbacks.add(courseFeedback);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         // Placeholder data from res/values/strings.xml
-        List<Model> ModelIs = createPlaceholderCourse();
+        List<Model> ModelIs = createPlaceholderCourse(courseNames,courseDates,courseFeedbacks);
+
+
 
         adapter = new AA_recyclerViewAdapter(getActivity(), ModelIs);
         recyclerView.setAdapter(adapter);
@@ -53,20 +87,17 @@ public class courselistingFragment extends Fragment {
         return view;
     }
 
-    private List<Model> createPlaceholderCourse() {
+    private List<Model> createPlaceholderCourse(List<String> courseNames, List<String> courseDates, List<String> courseFeedbacks) {
         List<Model> placeHolderCourse = new ArrayList<>();
-        String[] courseName = getResources().getStringArray(R.array.TVcourseName);
-        String[] courseDate = getResources().getStringArray(R.array.TVcourseDate);
-        String[] courseFeedback = getResources().getStringArray(R.array.TVcourseFeedback);
 
-        for (int i = 0; i < courseName.length &&
-                i < courseDate.length &&
-                i < courseFeedback.length; i++) {
-            Model course = new Model(courseName[i], courseDate[i], courseFeedback[i]);
+        int minLength = Math.min(Math.min(courseNames.size(), courseDates.size()), courseFeedbacks.size());
+
+        for (int i = 0; i < minLength; i++) {
+            Model course = new Model(courseNames.get(i), courseDates.get(i), courseFeedbacks.get(i));
             placeHolderCourse.add(course);
         }
 
         return placeHolderCourse;
+    }
 
-
-    }}
+}
