d7e5c967b90db45f4ec2b2fb7c435395a3bd92bc
Navid <navidrazavi22@gmail.com>
2023-12-23 22:39:58 +0800

    add stories and comment on each
    created the functionality to add stories and also put comments under each story. removed share button because we do not need it.

--- app/src/main/java/com/example/equalopportuna/StoriesForumFragment.java
+++ app/src/main/java/com/example/equalopportuna/StoriesForumFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
@@ -11,20 +13,27 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class StoriesForumFragment extends Fragment implements ForumPostAdapter.OnCommentButtonClickListener {
 
     private ForumPostAdapter adapter;
     private StoryManager storyManager;
+    private UserManager userManager;
+    private List<ForumPostNew> forumPosts;
 
     public StoriesForumFragment() {
         // Required empty public constructor
+    }
+    public void setForumPosts(List<ForumPostNew> forumPosts) {
+        this.forumPosts = forumPosts;
     }
 
     @Override
     public void onCommentButtonClick(int position, View view) {
         ForumPostNew currentPost = adapter.getItem(position);
+
 
         // Create a Bundle to pass data to the fragment
         Bundle bundle = new Bundle();
@@ -42,6 +51,8 @@ public class StoriesForumFragment extends Fragment implements ForumPostAdapter.O
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stories_forum, container, false);
 
+        userManager = UserManager.getInstance(requireContext());
+
         storyManager = StoryManager.getInstance(getContext());
 
         RecyclerView recyclerView = view.findViewById(R.id.ForumRecycler);
@@ -53,6 +64,53 @@ public class StoriesForumFragment extends Fragment implements ForumPostAdapter.O
         adapter.setOnCommentButtonClickListener(this); // Set the listener
         recyclerView.setAdapter(adapter);
 
+        adapter.setOnCommentButtonClickListener(this); // Set the listener
+
+
+        Button btnSend = view.findViewById(R.id.BTNsend);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Insert a new story into the database
+                Database database = new Database();
+                Connection connection = database.SQLConnection();
+                insertStory();
+                storyManager.fetchAndStoreStoryData(requireContext(), connection);
+
+
+                // Notify the adapter that the data has changed
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
+
         return view;
     }
+
+    public void insertStory() {
+        // Get the message from the EditText
+        EditText etMessage = requireView().findViewById(R.id.ETMessage);
+        String message = etMessage.getText().toString().trim();
+
+        // Insert the story into the database
+        if (!message.isEmpty()) {
+            Database database = new Database();
+            String username = userManager.getFullName(); // Assuming you have userManager initialized
+            database.insertStory(username, message);
+
+            // Clear the EditText after sending the story
+            etMessage.getText().clear();
+
+            // You may also want to update your RecyclerView with the new data
+            // Assuming StoryManager.getStoryList() gets the updated list of stories
+            List<ForumPostNew> updatedForumPosts = StoryManager.getStoryList();
+            adapter.setForumPosts(updatedForumPosts);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+
 }
