cd8f43cf6e45a76dae71abd77f6f49bd1b831099
Navid <navidrazavi22@gmail.com>
2023-12-30 21:22:19 +0800

    completed courses module
    removed some of the files that were not used and made the courses work with the firebase database so it is much faster now

--- app/src/main/java/com/example/equalopportuna/Model.java
+++ app/src/main/java/com/example/equalopportuna/Model.java
@@ -4,13 +4,20 @@ public class Model {
     private String courseName;
     private String courseDate;
     private String courseFeedback;
+    private String link;
+    private String description;
+
+    private String recommended;
     int image;
 
 
-    public Model(String courseName, String courseDate, String courseFeedback) {
+    public Model(String courseName, String courseDate, String courseFeedback, String link, String description, String recommendedCourse) {
         this.courseName = courseName;
         this.courseDate = courseDate;
         this.courseFeedback = courseFeedback;
+        this.link = link;
+        this.description = description;
+        this.recommended = recommendedCourse;
 
     }
 
@@ -19,10 +26,11 @@ public class Model {
     public String getCourseName(){return courseName;}
     public String getCourseDate(){return courseDate;}
     public String getCourseFeedback(){return courseFeedback;}
+    public String getLink(){return link;}
+    public String getDescription(){return description;}
+    public String getRecommendedCourse() { return recommended;}
 
-    public int getImage() {
-        return image;
-    }
+
 
 }
 
--- app/src/main/java/com/example/equalopportuna/activity_course_details.java
+++ app/src/main/java/com/example/equalopportuna/activity_course_details.java
@@ -1,74 +1,98 @@
-package com.example.equalopportuna;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-
-
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class activity_course_details extends AppCompatActivity {
-
-
-
-    String link;
-    String description;
-    String name;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_course_details);
-        int receivedValue = 0;
-        // Receive the passed value from the previous activity
-        if (getIntent().hasExtra("id")) {
-            receivedValue = getIntent().getIntExtra("id",0); // Replace "YOUR_KEY" with your key
-        }
-        System.out.println(receivedValue);
-
-        Database database = new Database();
-        Connection connection = database.SQLConnection();
-        String query = "SELECT course_name, description, youtube_link FROM courses where id = " + receivedValue ;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                // 获取每一行的数据
-                link = resultSet.getString("youtube_link");
-                description = resultSet.getString("description");
-                name  = resultSet.getString("course_name");
-                System.out.println(description);
-                TextView heading = findViewById(R.id.heading);
-                TextView content = findViewById(R.id.description);
-                heading.setText(name);
-                content.setText(description);
-                Button watch = findViewById(R.id.watchBtn);
-                watch.setOnClickListener(new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        Uri uri = Uri.parse(link);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                        startActivity(intent);
+    package com.example.equalopportuna;
+    
+    import android.content.Context;
+    import android.content.Intent;
+    import android.content.SharedPreferences;
+    import android.net.Uri;
+    import android.os.Bundle;
+    import android.widget.Button;
+    import android.widget.SeekBar;
+    import android.widget.TextView;
+    import androidx.annotation.Nullable;
+    import androidx.appcompat.app.AppCompatActivity;
+    
+    public class activity_course_details extends AppCompatActivity {
+    
+        String link;
+        String description;
+        String name;
+        String recommended;
+        String username;
+    
+        // SharedPreferences key for storing progress
+        private static final String PREF_PROGRESS_PREFIX = "progress_";
+    
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_course_details);
+    
+            // Retrieve data from intent extras
+            Intent intent = getIntent();
+            if (intent != null) {
+                name = intent.getStringExtra("name");
+                link = intent.getStringExtra("link");
+                description = intent.getStringExtra("description");
+                recommended = intent.getStringExtra("recommended");
+                username = intent.getStringExtra("username");
+    
+                // Set values to TextViews
+                TextView tvHeading = findViewById(R.id.heading);
+                TextView tvDescription = findViewById(R.id.description);
+                TextView tvTypeValue = findViewById(R.id.type_value);
+                TextView tvRecommend = findViewById(R.id.recommendation);
+    
+                tvHeading.setText(name);
+                tvDescription.setText(description);
+                tvRecommend.setText(recommended);
+                tvTypeValue.setText("Video");
+    
+                // Watch Youtube button click listener
+                Button watchBtn = findViewById(R.id.watchBtn);
+                watchBtn.setOnClickListener(view -> {
+                    if (link != null && !link.isEmpty()) {
+                        // Open the Youtube link
+                        Intent youtubeIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                        startActivity(youtubeIntent);
                     }
                 });
             }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+    
+            // SeekBar for progress tracking
+            SeekBar progressSlider = findViewById(R.id.progressSlider);
+            int savedProgress = getSavedProgress(); // Retrieve saved progress
+            progressSlider.setProgress(savedProgress);
+            progressSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    // Update the progress as the user slides the SeekBar
+                    // You may want to display the progress in a TextView or handle it as needed
+                }
+    
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    // Handle the start of tracking (if needed)
+                }
+    
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    // Save the final progress when the user stops sliding
+                    saveProgress(seekBar.getProgress(), name); // Save progress with course name as key
+                }
+            });
         }
 
+        private int getSavedProgress() {
+            // Retrieve and return the user's saved progress for the current course and username
+            SharedPreferences preferences = getSharedPreferences("progress_pref_" + username, Context.MODE_PRIVATE);
+            return preferences.getInt(PREF_PROGRESS_PREFIX + name, 0);
+        }
+
+        private void saveProgress(int progress, String courseName) {
+            // Save the user's progress for the current course and username
+            SharedPreferences preferences = getSharedPreferences("progress_pref_" + username, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt(PREF_PROGRESS_PREFIX + courseName, progress);
+            editor.apply();
+        }
     }
-
-
-
-}
